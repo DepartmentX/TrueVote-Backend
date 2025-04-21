8ba9cd22b89941d1e0ad4a12be776f667e95b219
niduniDK <nidunikasige@gmail.com>
2025-04-21 22:52:03 +0530

    Create API to integrate stacked model

--- fraud_detection_api/api/predict.py
+++ fraud_detection_api/api/predict.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from schema import ElectionFraudDetectionResponse
+import pandas as pd
 from model import stacked_model_predict
 
 app = APIRouter()
@@ -12,3 +13,13 @@ def validate_vote(data):
         return ElectionFraudDetectionResponse(Address=fraud_pred['Address'], is_fraud=fraud_pred['is_fraud'])
     else:
         raise ValueError("Invalid data format or prediction failed.")
+    
+
+print(validate_vote({
+  "Address": "0x3f5CE5FBFe3E9af3971dD833D26BA9b5C936f0bE",
+  "Time Diff between first and last (Mins)": 12,
+  "Face Attempts": 5,
+  "Detected As a Robot At Least Once": 0,
+  "Face Match Percentage": 94.2,
+  "Liveness Score of The Face": 0.93
+}))
