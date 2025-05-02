d1b52747c610e41d243f9a26b19a052977fec749
niduniDK <nidunikasige@gmail.com>
2025-05-02 17:41:59 +0530

    Integrate meta model

--- backend/fraud_detection_api/api/model.py
+++ backend/fraud_detection_api/api/model.py
@@ -7,8 +7,8 @@ from Crypto.Hash import keccak
 from schema import ElectionFraudDetectionRequest
 
 iso_foret_model = joblib.load('../model/isolation_forest_model.pkl')
-# logistic_regression_model = LogisticRegression()     # need to load the actual model
-# meta_model = XGBClassifier()
+logistic_regression_model = joblib.load('../model/logistic_regression_model.pkl')
+meta_model = joblib.load('../model/model_xgb.joblib')
 
 def hash_address(address):
     keccak_hash = keccak.new(digest_bits=256)
@@ -29,19 +29,17 @@ def stacked_model_predict(data):
     print(pre_processed_data.columns)
 
     if not data:
-        raise ValueError("No data provided for prediction.")    
+        raise ValueError("No data provided for prediction.")
+    
     iso_forest_preds = iso_foret_model.predict(pre_processed_data)
-    # logistic_regression_preds = logistic_regression_model.predict(data)
+    logistic_regression_preds = logistic_regression_model.predict_proba(pre_processed_data.drop(columns=['Address']).to_numpy())
     
     meta_dataset = pd.DataFrame({
-        'Address': data.Address,
-        'Isolation_Forest_Prediction': iso_forest_preds[0],
-        # 'Logistic_Regression_Prediction': logistic_regression_preds['FLAG']
+        'is_anomaly': iso_forest_preds[0],
+        'fraud_probability': logistic_regression_preds[0][1],
     }, index=[0]).to_numpy()
 
-    # is_fraud = meta_model.predict(meta_dataset)
-
-    is_fraud = iso_forest_preds  # Placeholder for the actual meta model prediction, need to update
+    is_fraud = meta_model.predict(meta_dataset)
 
     if is_fraud[0] == 1:
         return {'Address': data.Address, 'is_fraud': True}
