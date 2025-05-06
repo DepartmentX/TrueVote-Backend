903cfe230ca356765dc018881f0e9bb1f776f75b
mickymarsh <162584880+mickymarsh@users.noreply.github.com>
2025-05-06 21:54:55 +0530

    recaptcha part done

--- backend/fraud_detection_api/api/predict.py
+++ backend/fraud_detection_api/api/predict.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from schema import ElectionFraudDetectionResponse, ElectionFraudDetectionRequest
 import pandas as pd
 from model import stacked_model_predict
+from pydantic import BaseModel
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("RECAPTCHA_SECRET_KEY")
 
 app = FastAPI()
 
@@ -14,6 +22,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+class CaptchaRequest(BaseModel):
+    token: str
 
 @app.post("/validate_vote/", response_model=ElectionFraudDetectionResponse)
 def validate_vote(data: ElectionFraudDetectionRequest):
@@ -28,3 +39,16 @@ def validate_vote(data: ElectionFraudDetectionRequest):
             raise HTTPException(status_code=400, detail="Prediction failed.")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/verify-captcha")
+async def verify_captcha(data: CaptchaRequest):
+    response = requests.post(
+        "https://www.google.com/recaptcha/api/siteverify",
+        data={
+            "secret": SECRET_KEY,
+            "response": data.token
+        }
+    )
+    result = response.json()
+    return result
